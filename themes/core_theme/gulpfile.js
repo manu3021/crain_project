9d068ce3cb045333129e82710d3db1919767b15f
Sumit kumar <sumit.kumar@srijan.net>
2017-09-12 10:26:39 +0530

    modified gulp

--- themes/core_theme/gulpfile.js
+++ themes/core_theme/gulpfile.js
@@ -1,331 +1,130 @@
-//=======================================================
-// Configurations & Options
-//=======================================================
-var configGulp = require('./gulp.config')();
+'use strict';
 
-//=======================================================
-// Declaration node modules
-//=======================================================
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var sourcemaps  = require('gulp-sourcemaps');
-var sassLint    = require('gulp-sass-lint');
-var eslint      = require('gulp-eslint');
-var babel       = require('gulp-babel');
-var rename      = require('gulp-rename');
-var imagemin    = require('gulp-imagemin');
-var concat      = require('gulp-concat');
-var gfile       = require('gulp-file');
-var replace     = require('gulp-replace');
-var insert      = require('gulp-insert');
-var inject      = require('gulp-inject');
-var notify      = require('gulp-notify');
-var plumber     = require('gulp-plumber');
-var watch       = require('gulp-watch');
-var del         = require('del');
-var runSequence = require('run-sequence');
-var kss         = require('kss');
-var fs          = require('fs');
-var path        = require('path');
-var wiredep     = require('wiredep').stream;
-var notifier    = require('node-notifier');
-var _           = require('lodash');
-var sassGlob = require('gulp-sass-glob');
+var gulp = require('gulp'),
+    path = require('path'),
+    autoprefixer = require('autoprefixer'),
+    kss = require('kss'),
+    loadPlugins = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create(),
+    fontAwesome = require('node-font-awesome');
 
-//=======================================================
-// Main Tasks
-//=======================================================
-gulp.task('default', function(doneCallback) {
-  runSequence(
-    'build',
-    function() {
-      notifyDoneTasks();
-      doneCallback();
-    }
-  );
-});
+var paths = {
+  styles: {
+    source: 'scss/',
+    destination: 'css/'
+  },
+  scripts: 'js/',
+  images: 'images/',
+  styleGuide: 'styleguide',
+  templates: 'templates/'
+};
 
-gulp.task('serve', function(doneCallback) {
-  runSequence(
-    'build',
-    'watch',
-    function() {
-      notifyDoneTasks();
-      doneCallback();
-    }
-  );
-});
+// error notifications
+var handleError = function (task) {
+  return function (err) {
+    loadPlugins.util.beep();
 
-gulp.task('build', function(doneCallback) {
-  runSequence(
-    'clean',
-    'build:all',
-    ['lint', 'copy:assets', 'build:kss-styleguide'],
-    function() {
-      notifyDoneTasks();
-      doneCallback();
-    }
-  );
-});
+    loadPlugins.notify.onError({
+      title: task + err.plugin,
+      message: err.message,
+      sound: true,
+      icon: false
+    })(err);
 
-gulp.task('build-prod', function() {
-  runSequence(
-    'clean',
-    'build:all',
-    ['lint', 'compress:assets'],
-    notifyDoneTasks
-  );
-});
+    loadPlugins.util.log(loadPlugins.util.colors.bgRed(task + ' error:'), loadPlugins.util.colors.red(err));
+    this.emit('end');
+  };
+};
 
-gulp.task('build:all', [
-  'compile:sass',
-  'transpile:common'
-]);
-
-//=======================================================
-// Watch
-//=======================================================
-gulp.task('watch', function() {
-  var listAllCSS = [];
-  listAllCSS.push(configGulp.globs.scss.all, configGulp.globs.css.all);
-
-  watch(configGulp.globs.js.all, function() {
-    runSequence(['lint:js', 'compile:js'], notifyRunningTasks);
-  });
-
-  watch(listAllCSS, function() {
-    runSequence(['lint:sass', 'compile:sass'], notifyRunningTasks);
-  });
-
-  watch(configGulp.globs.js.all, function() {
-    runSequence(['lint:js', 'compile:js'], notifyRunningTasks);
-  });
-
-  watch([
-    configGulp.globs.twig.all,
-    configGulp.globs.json.all,
-    './style-guide/builder/kss-assets/kss.css'
-  ], function() {
-    runSequence(['clean:kss-styleguide', 'build:kss-styleguide'], notifyRunningTasks);
+// Task for the browserSync
+gulp.task('browserSync', function() {
+  //watch files
+  loadPlugins.util.log(loadPlugins.util.colors.yellow('BrowserSync starting'));
+  var files = [
+    paths.styles.destination + '**/*.css',
+    paths.scripts + '**/*.js',
+    paths.images + '**/*',
+    paths.templates + '**/*.twig'
+  ];
+  browserSync.init({
+    files: files,
+    injectChanges: true,
+    proxy: "dev.vodafone.com",
+    online: true
   });
 });
 
-//=======================================================
-// Compile
-//=======================================================
-gulp.task('compile:sass', function() {
-  return gulp.src(configGulp.globs.scss.all)
-    .pipe(sourcemaps.init())
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(sass({
-      outputStyle: 'nested'
-    })
-    .on('error', sass.logError))
-    .pipe(prefix({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(rename(function (path) {
-      path.dirname = '';
-      return path;
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/css'));
+//Task to compile the sass, linting, sourcemaps
+gulp.task('sass', function() {
+  loadPlugins.util.log(loadPlugins.util.colors.yellow('Compiling the theme CSS!'));
+  var plugins = [
+    autoprefixer({browsers: ['last 4 version']})
+  ];
+  const SASS_INCLUDE_PATHS = [
+    path.join(__dirname, '/node_modules/susy/sass'),
+    fontAwesome.scssPath
+  ];
+  return gulp.src(paths.styles.source + '**/*.scss')
+  .pipe(loadPlugins.plumber({errorHandler: handleError('Gulp Error in')}))
+  .pipe(loadPlugins.sassGlob())
+  .pipe(loadPlugins.scssLint())
+  .pipe(loadPlugins.sourcemaps.init({loadMaps: true}))
+  .pipe(loadPlugins.sass({
+    includePaths: SASS_INCLUDE_PATHS
+  })) // Converts Sass to CSS with gulp-sass
+  .pipe(loadPlugins.postcss(plugins))
+  .pipe(loadPlugins.sourcemaps.write('.'))
+  .pipe(gulp.dest(paths.styles.destination))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
-gulp.task('transpile:components', function() {
-  return gulp.src(configGulp.globs.js.components)
-    .pipe(sourcemaps.init())
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(babel({
-      presets: ['es2015'],
-      compact: true
-    }))
-    .pipe(insert.append('\nif(typeof init == \'function\') { init(); }'))
-    .pipe(rename(function (path) {
-      path.dirname = '';
-      return path;
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/scripts/cfr_modules'));
+// Task to lint the JS
+// gulp.task('lint', function () {
+//   loadPlugins.util.log(loadPlugins.util.colors.yellow('Reviewing JavaScript files!'));
+//   return gulp.src([paths.scripts + 'script.js','!node_modules/**'])
+//   .pipe(loadPlugins.eslint())
+//   .pipe(loadPlugins.eslint.format())
+//   .pipe(loadPlugins.eslint.failAfterError().on('error',loadPlugins.util.log))
+//   .pipe(loadPlugins.uglify())
+//   .pipe(gulp.dest(paths.scripts))
+//   .pipe(browserSync.reload({
+//     stream: true
+//   }))
+// });
+
+// Task to optimize the images
+gulp.task('images', function(){
+  loadPlugins.util.log(loadPlugins.util.colors.yellow('Optimizing images!'));
+  return gulp.src(paths.images + '**/*.+(png|jpg|gif|svg)')
+  .pipe(loadPlugins.cache(loadPlugins.imagemin()))
+  .pipe(gulp.dest(paths.images))
 });
 
-gulp.task('transpile:common', function() {
-  return gulp.src(configGulp.globs.js.common)
-    .pipe(sourcemaps.init())
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')}
-    ))
-    .pipe(babel({
-      presets: ['es2015'],
-      compact: true
-    }))
-    .pipe(concat('common.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js'));
-});
-
-
-//=======================================================
-// Lint
-//=======================================================
-gulp.task('lint', ['lint:sass', 'lint:js']);
-
-gulp.task('lint:sass', function () {
-  // [FS] NOTE: Configuration within .sass-lint.yml
-  return gulp.src(configGulp.globs.scss.all)
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(sassLint())
-    .pipe(sassLint.format());
-});
-
-gulp.task('lint:js', function () {
-  // [FS] NOTE: Configuration within .eslintrc
-  return gulp.src([
-    './src/components/**/*.js'
-  ])
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
-
-//=======================================================
-// Generate style guide
-//=======================================================
-gulp.task('compile:kss-styleguide', function() {
+// Task to generate the styleguide
+gulp.task('styleguide', function(){
+  var css = [
+    '../css/style.css'
+  ];
   return kss({
-    source: [
-      'src/components',
-      'src/styles'
-    ],
-    destination: './dist/style-guide',
-    builder: 'style-guide/builder',
-    namespace: [
-      'core_theme:' + __dirname + '/src/components/',
-      'kss_utils:' + __dirname + '/gulp/templates/kss-utils/'
-    ],
-    'extend-drupal8': true,
-    css: [
-      path.relative(
-        __dirname + '/style-guide/',
-        __dirname + '/css/main.css'
-      )
-    ],
-    js: [],
-    homepage: 'style-guide.md',
-    title: 'Style Guide For Crain'
-  });
+    title: 'Core theme Styleguide',
+    source: paths.styles.source,
+    destination: paths.styleGuide,
+    homepage: 'readme.md',
+    css: css
+    });
 });
 
-gulp.task('build:kss-styleguide', function(doneCallback) {
-  runSequence(
-    'compile:kss-styleguide',
-    'inject:js:kss-styleguide',
-    doneCallback
-  );
+// Watch Task
+gulp.task('watch', ['sass',  'browserSync'], function (){
+  gulp.watch(paths.styles.source + '**/*.scss', ['sass']);
+  gulp.watch(paths.scripts + '**/*.js', ['lint']);
+  gulp.watch(paths.templates + '**/*.twig', browserSync.reload);
+})
+
+// Task to test the CSS
+gulp.task('css:test', function(){
+  return gulp.src(paths.styles.destination + '*.css')
+  .pipe(loadPlugins.parker());
 });
-
-gulp.task('compile:js', function(doneCallback) {
-  runSequence(
-    'transpile:common',
-    'compile:kss-styleguide',
-    'inject:js:kss-styleguide',
-    doneCallback
-  );
-});
-
-//=======================================================
-// Copy, Preprocess, Compress, Injection Tasks
-//=======================================================
-gulp.task('inject:js:kss-styleguide', function() {
-  return gulp.src('./dist/style-guide/**/*.html')
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(wiredep(configGulp.optionsWiredep))
-    .pipe(inject(gulp.src('./dist/{js,drupal-js-libraries}/*.js', {
-      read: false
-    }), {
-      relative: true,
-      quiet: true
-    }))
-    .pipe(gulp.dest('./dist/style-guide'));
-});
-
-gulp.task('copy:assets', function() {
-  return gulp.src(configGulp.globs.assets.all)
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(rename(renamingAssetsStrategy))
-    .pipe(gulp.dest('./dist/assets'));
-});
-
-gulp.task('compress:assets', function() {
-  return gulp.src(configGulp.globs.assets.imagesOnly)
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false
-      }]
-    }))
-    .pipe(rename(renamingAssetsStrategy))
-    .pipe(gulp.dest('./dist/assets'));
-});
-
-//=======================================================
-// Clean
-//=======================================================
-gulp.task('clean', function () {
-  return del([
-    './dist/*'
-  ], {
-    force: true
-  });
-});
-
-gulp.task('clean:kss-styleguide', function () {
-  return del([
-    './dist/style-guide/*'
-  ], {
-    force: true
-  });
-});
-
-//=======================================================
-// Helpers & Utils
-//=======================================================
-function renamingAssetsStrategy(path) {
-  if (_.includes(path, 'fonts')) {
-    return path;
-  }
-
-  path.dirname = '';
-  return path;
-}
-
-function notifyRunningTasks() {
-  return notifier.notify({
-    title: 'Watch',
-    message: 'running tasks...'
-  });
-}
-
-function notifyDoneTasks() {
-  return notifier.notify({
-    title: 'Build',
-    message: 'Done'
-  });
-}
